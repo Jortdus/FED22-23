79bd44b1000bf0cb19236bb3ad57a9d8b60b29c8
Jort Broekhof <jort@acato.nl>
2023-01-10 21:11:29 +0100

    header

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function lightCheck() {
+    document.documentElement.dataset.colorMode = "";
+}
+
+function darkCheck() {
+    document.documentElement.dataset.colorMode = "dark-mode";
+}
+
+function contrastCheck() {
+    document.documentElement.dataset.colorMode = "contrast-mode";
+}
