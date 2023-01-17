8a6448f157c532346d1736e84eb8b56bbbc744d1
Jortdus <jortdus@gmail.com>
2023-01-17 01:08:16 +0100

    js fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
-function lightCheck() {
+let lightButton = document.querySelector("header ul li");
+let darkButton = document.querySelector("header ul li:nth-of-type(2)");
+let contrastButton = document.querySelector("header ul li:nth-of-type(3)");
+
+lightButton.addEventListener('click', function handleClick() {
+    console.log('lightmode clicked');
     document.documentElement.dataset.colorMode = "";
-}
+});
 
-function darkCheck() {
+darkButton.addEventListener('click', function handleClick() {
+    console.log('darkmode clicked');
     document.documentElement.dataset.colorMode = "dark-mode";
-}
+});
 
-function contrastCheck() {
+contrastButton.addEventListener('click', function handleClick() {
+    console.log('contrastmode clicked');
     document.documentElement.dataset.colorMode = "contrast-mode";
-}
+});
+
